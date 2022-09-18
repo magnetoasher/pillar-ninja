a27a368adeb8dad3c23acf354b2471867a2f2923
magnetoasher <magnetoasher@gmail.com>
2022-09-17 19:55:38 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-screen p-5">Hello world!</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped></style>
